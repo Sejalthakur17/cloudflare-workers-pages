bbfd1ba3e51114fa4257ed8ad74ac6af7b2da67d
Sejal Thakur <sejalthakurcs@gmail.com>
2026-03-02 21:36:43 +0530

    Added Terraform

--- worker/index.js
+++ worker/index.js
@@ -1,26 +1,7 @@
 addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request));
+  event.respondWith(
+    new Response("Hello from Backend!", {
+      headers: { "Content-Type": "text/plain" }
+    })
+  );
 });
-
-async function handleRequest(request) {
-
-  // Handle preflight requests
-  if (request.method === "OPTIONS") {
-    return new Response(null, {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-        "Access-Control-Allow-Headers": "*"
-      }
-    });
-  }
-
-  return new Response("Hello from Sejal Backend!", {
-    headers: {
-      "Content-Type": "text/plain",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-      "Access-Control-Allow-Headers": "*"
-    },
-  });
-}
